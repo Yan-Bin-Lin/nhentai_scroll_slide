c73b1476399e4a8c5a3aaeb7c31450b5c33dec50
Yan-Bin-Lin <beyond1generation@gmail.com>
2021-02-10 01:48:16 +0800

    Add files via upload

---
+++ Nhentai scroll.user.js
@@ -0,0 +1,53 @@
+// ==UserScript==
+// @name         Nhentai scroll
+// @namespace    https://github.com/Yan-Bin-Lin/nhentai_scroll_slide
+// @version      0.1(
+// @description  scroll to read picture in nhentai
+// @author       Lin Yan Bin
+// @match        https://nhentai.net/g/*/*/
+// @require      https://code.jquery.com/jquery-3.5.1.min.js
+// @grant        none
+// ==/UserScript==
+(function() {
+    let ic = $("#image-container");
+    let num = parseInt($("span.num-pages").first().text());
+    let img = ic.find("img");
+    let src = img.attr("src");
+    let base = src.slice(0, src.lastIndexOf("/") + 1);
+    let width = img.attr("width");
+    let height = img.attr("height");
+    let imgs = [];
+    for (let i = 1;i <= num; i++){
+        imgs.push(base + i.toString() + '.jpg')
+        //ic.append('<img src="' + base + i.toString() + '.jpg" width="' + width + '" height="' + height + '">')
+    }
+
+    function loadImage(imgs, width, height, target) {
+        if (!imgs.length) {
+            return;
+        }
+
+        let url = imgs.shift();
+        $('<img src="'+ url +'">').on("load", function() {
+            $(this).width(width).height(height).appendTo(target);
+            loadImage(imgs, width, height, target)
+        });
+    }
+
+    loadImage(imgs, width, height, '#image-container');
+/*
+    function loadImagesInSequence(images) {
+        if (!images.length) {
+            return;
+        }
+
+        let img = new Image();
+        let url = images.shift();
+
+        img.onload = function(){ loadImagesInSequence(images) };
+        img.src = url;
+    }
+
+    loadImagesInSequence(imgs);
+*/
+})();
